bc47ac9491aba30c012f168ec58a68f31a00b975
Hana <andywangsy@gmail.com>
2022-05-17 15:40:09 +0800

    feat: support tsfn return (#2)
    * feat(napi): impl threadsafe function with return value
    
    * test(napi): add test for tsfn with return
    
    * fix: fix promise return
    
    * feat: simplify example cases
    
    * fix: fix feature-based use

--- examples/napi-compat-mode/src/napi4/tsfn_dua_instance.rs
+++ examples/napi-compat-mode/src/napi4/tsfn_dua_instance.rs
@@ -1,5 +1,5 @@
 use napi::{
-  threadsafe_function::{ThreadSafeCallContext, ThreadsafeFunction},
+  threadsafe_function::{ThreadSafeCallContext, ThreadSafeResultContext, ThreadsafeFunction},
   CallContext, JsFunction, JsObject, JsUndefined,
 };
 use napi_derive::js_function;
@@ -13,15 +13,17 @@ pub struct A {
 pub fn constructor(ctx: CallContext) -> napi::Result<JsUndefined> {
   let callback = ctx.get::<JsFunction>(0)?;
 
-  let mut cb =
-    ctx
-      .env
-      .create_threadsafe_function(&callback, 0, |ctx: ThreadSafeCallContext<String>| {
-        ctx
-          .env
-          .create_string_from_std(ctx.value)
-          .map(|js_string| vec![js_string])
-      })?;
+  let mut cb = ctx.env.create_threadsafe_function(
+    &callback,
+    0,
+    |ctx: ThreadSafeCallContext<String>| {
+      ctx
+        .env
+        .create_string_from_std(ctx.value)
+        .map(|js_string| vec![js_string])
+    },
+    |_: ThreadSafeResultContext<JsUndefined>| (),
+  )?;
 
   cb.unref(ctx.env)?;
 
--- examples/napi/index.d.ts
+++ examples/napi/index.d.ts
@@ -240,7 +240,6 @@ export class Bird {
 export type Blake2bHasher = Blake2BHasher
 /** Smoking test for type generation */
 export class Blake2BHasher {
-  static withKey(key: Blake2bKey): Blake2BHasher
   update(data: Buffer): void
 }
 export type Blake2bKey = Blake2BKey
@@ -307,7 +306,6 @@ export class AnotherClassForEither {
   constructor()
 }
 export class Fib {
-  [Symbol.iterator](): Iterator<number, void, number>
   constructor()
 }
 export class Fib2 {

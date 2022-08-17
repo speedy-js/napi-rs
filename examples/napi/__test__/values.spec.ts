4e2bd5473aa35ec199abc80cfefa13d50f9a4349
LongYinan <lynweklm@gmail.com>
2022-08-17 17:49:22 +0800

    feat(napi-derive): support inject This<Value> into raw function

--- examples/napi/__test__/values.spec.ts
+++ examples/napi/__test__/values.spec.ts
@@ -109,6 +109,8 @@ import {
   getNumArr,
   getNestedNumArr,
   CustomFinalize,
+  plusOne,
+  Width,
 } from '../'
 
 test('export const', (t) => {
@@ -162,7 +164,7 @@ test('enum', (t) => {
   t.is(enumToI32(CustomNumEnum.Eight), 8)
 })
 
-test('class', (t) => {
+test.only('class', (t) => {
   const dog = new Animal(Kind.Dog, '旺财')
 
   t.is(dog.name, '旺财')
@@ -193,6 +195,13 @@ test('class', (t) => {
   const turtle = NinjaTurtle.newRaph()
   t.is(turtle.returnThis(), turtle)
   t.is(NinjaTurtle.isInstanceOf(turtle), true)
+  // Inject this to function
+  const width = new Width(1)
+  t.is(plusOne.call(width), 2)
+  t.throws(() => {
+    // @ts-expect-error
+    plusOne.call('')
+  })
 })
 
 test('class factory', (t) => {
